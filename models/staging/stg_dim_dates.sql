73316dbd5f0b29b1b48e07c58448fc6e7201c993
mattheus  nascimento <mmsnascimento23@gmail.com>
2024-10-27 14:55:14 +0000

    add dim_dates

--- models/staging/stg_dim_dates.sql
+++ models/staging/stg_dim_dates.sql
@@ -6,12 +6,28 @@ with
 
 , generate_date as (
     select
-        raw_generate_date.date_day
-      , cast(raw_generate_date.year_number as int ) as "year"
-      , raw_generate_date.month_name as "month"
+        raw_generate_date.date_day as date_day
+      , raw_generate_date.year_number as year_number
+      , case 
+            when raw_generate_date.year_number = 2011 then '2011-01-01' 
+            when raw_generate_date.year_number = 2012 then '2012-01-01'
+            when raw_generate_date.year_number = 2013 then '2013-01-01'
+            when raw_generate_date.year_number = 2014 then '2014-01-01'
+        end as year_date
+      , raw_generate_date.month_name as date_month
       , concat (raw_generate_date.month_name_short||'/'||raw_generate_date.year_number) as year_month
       , cast(raw_generate_date.quarter_of_year as int) as quarter   
     from raw_generate_date
 )
 
-select * from generate_date
+, final_date as (
+    select 
+        date_day
+        , cast( year_date as date) as year_date
+        , date_month
+        , year_month
+        , quarter
+    from generate_date
+)
+
+select * from final_date
